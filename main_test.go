a93a5c4849a18fbbc1eb948ccbc960802ff71905
Mat <malvin888@gmail.com>
2023-04-24 10:33:26 +0000

    added tests for getProducts

--- main_test.go
+++ main_test.go
@@ -87,3 +87,66 @@ func TestGetUsers(t *testing.T) {
 
 	// todo more chcecks
 }
+
+//test for getProduct
+
+func TestGetProducts(t *testing.T) {
+	// Define a mock the response based on first two users from the API
+	mockResponse := `[
+		{
+		  "id": 1,
+		  "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+		  "price": 109.95,
+		  "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+		  "category": "men's clothing",
+		  "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+		  "rating": {
+			"rate": 3.9,
+			"count": 120
+		  }
+		},
+		{
+		  "id": 2,
+		  "title": "Mens Casual Premium Slim Fit T-Shirts ",
+		  "price": 22.3,
+		  "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+		  "category": "men's clothing",
+		  "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+		  "rating": {
+			"rate": 4.1,
+			"count": 259
+		  }
+		}]`
+
+	// Create a mock HTTPS server
+	mockServer := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(mockResponse))
+	}))
+	defer mockServer.Close()
+	// fix for error with url (parse the mock server URL)
+	mockServerURL, _ := url.Parse(mockServer.URL)
+
+	// Create a custom HTTP client with the server's certificate
+	mockClient := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: mockServer.Client().Transport.(*http.Transport).TLSClientConfig,
+			Proxy:           http.ProxyURL(mockServerURL),
+		},
+	}
+
+	// Call getProducts with the custom HTTP client
+	products, err := getProducts(mockClient)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+
+	// Check the length of the products slice
+	expectedLen := 2
+	if len(products) != expectedLen {
+		t.Errorf("Expected %d products, got %d", expectedLen, len(products))
+	}
+
+	// todo more chcecks
+}
